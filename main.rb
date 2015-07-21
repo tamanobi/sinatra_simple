fd67c63dec17e821bca2f4c70f5650a8843668c6
Kohki YAMAGIWA <tamanobi@gmail.com>
2015-07-22 02:51:22 +0900

    ユーザがテキストファイルを投稿できる

--- main.rb
+++ main.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'sqlite3'
+require 'pp'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
 
 get '/' do
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+  pp posts
   erb :index
+end
+
+post '/' do
+  stmt = db.prepare("INSERT INTO posts (text) VALUES(?)")
+  stmt.bind_params(params["ex_text"])
+  stmt.execute
+  redirect '/'
 end
 
 get '/hello' do
